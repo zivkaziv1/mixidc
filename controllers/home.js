7a5f48d49f4f0b77063ac7fcf32a94563a3a193c
Z95 <Kalziv@gmail.com>
2015-12-03 19:40:17 +0200

    make it better

---
+++ controllers/home.js
@@ -0,0 +1,15 @@
+/**
+ * GET /
+ * Home page.
+ */
+exports.index = function(req, res) {
+  res.render('home', {
+    title: 'Home'
+  });
+};
+
+exports.escapeVelocity = function(req, res) {
+  res.render('escape-velocity', {
+    title: 'Landing Page'
+  });
+};
